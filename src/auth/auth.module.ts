0ddf550d5cd5a17efb4607dfaf9cc894d5fa0de1
Nillud <agadullin.linar@mail.ru>
2026-01-27 10:02:07 +0500

    auth service, jwt config, graphql test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthResolver } from './auth.resolver';
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthResolver } from './auth.resolver'
+import { PrismaModule } from 'src/prisma/prisma.module'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+import { getJwtConfig } from 'src/config/jwt.config'
 
 @Module({
-  providers: [AuthService, AuthResolver]
+	imports: [
+		PrismaModule,
+		JwtModule.registerAsync({
+			imports: [PrismaModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	],
+	providers: [AuthService, AuthResolver]
 })
 export class AuthModule {}
